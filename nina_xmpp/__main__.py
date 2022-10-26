ebe05852eebc41ee3290ee7af23ca0def38dd111
milaq <micha.laqua@gmail.com>
2022-10-26 14:30:44 +0200

    remove log configuration and  argparse-logging requirement

--- nina_xmpp/__main__.py
+++ nina_xmpp/__main__.py
@@ -2,27 +2,9 @@ import asyncio
 import logging
 import logging.handlers
 
-from argparse_logging import add_log_level_argument, LoggingAction
-
 from . import NinaXMPP
 
-
-class LogHandler(LoggingAction):
-    def configure(self, path):
-        if path:
-            logging.getLogger().addHandler(
-                logging.handlers.SysLogHandler(address=path)
-            )
-
-
-def add_log_handler_argument(parser, option_string="--syslog"):
-    return parser.add_argument(
-        option_string,
-        help="Enable logging to syslog at given address",
-        metavar="ADDRESS",
-        type=str,
-        action=LogHandler,
-    )
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
 
 
 def main():
@@ -31,8 +13,6 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('config_file', type=argparse.FileType('r'))
-    add_log_level_argument(parser)
-    add_log_handler_argument(parser)
     args = parser.parse_args()
 
     config = yaml.safe_load(args.config_file)
